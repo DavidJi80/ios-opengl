4ae178975ed6f575acae6f236ea09fd2ba059a1f
David Ji <297933227@qq.com>
2020-09-03 11:26:40 +0800

    init

--- ios-opengl/AppDelegate.m
+++ ios-opengl/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -14,28 +15,28 @@
 
 @implementation AppDelegate
 
+-(void)navigationApp{
+    // 创建UIWindows对象
+    self.window=[[UIWindow alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
+    // 设置UIWindows的背景色
+    self.window.backgroundColor=[UIColor whiteColor];
+    // 创建ViewController
+    ViewController *vc=[[ViewController alloc]init];
+    // 创建导航控制器
+    UINavigationController * navc=[[UINavigationController alloc]initWithRootViewController:vc];
+    // 设置root view
+    self.window.rootViewController =navc;
+    // 设置window为主window且可见
+    [self.window makeKeyAndVisible];
+}
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self navigationApp];
     return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
 
 
 @end
