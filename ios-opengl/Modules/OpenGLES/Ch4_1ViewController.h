9eae7a4d905cbcac788ddab23c118bfa5a1c6d47
David Ji <297933227@qq.com>
2020-09-08 17:31:49 +0800

    GLSL

---
+++ ios-opengl/Modules/OpenGLES/Ch4_1ViewController.h
@@ -0,0 +1,17 @@
+//
+//  Ch4_1ViewController.h
+//  ios-opengl
+//
+//  Created by mac on 2020/9/5.
+//  Copyright © 2020 TongArk. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Ch4_1ViewController : GLKViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- ios-opengl/Modules/ViewController.m
+++ ios-opengl/Modules/ViewController.m
@@ -9,13 +9,19 @@
 #import "ViewController.h"
 #import "Ch2_1ViewController.h"
 #import "Ch3_1ViewController.h"
+#import "Ch4_1ViewController.h"
 #import "DrawTriangleViewController.h"
+#import "LightGLKVC.h"
+#import "GLSL1ViewController.h"
 
 @interface ViewController ()
 
 @property (strong, nonatomic) UIButton *ch2_1Btn;
 @property (strong, nonatomic) UIButton *ch3_1Btn;
+@property (strong, nonatomic) UIButton *ch4_1Btn;
 @property (strong, nonatomic) UIButton *demo1Btn;
+@property (strong, nonatomic) UIButton *demo2Btn;
+@property (strong, nonatomic) UIButton *demoGlsl1Btn;
 
 @end
 
@@ -40,12 +46,31 @@
     [self.view addSubview:self.ch3_1Btn];
     [self.ch3_1Btn makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.ch2_1Btn.right).offset(10);
-        make.top.height.width.equalTo(self.ch2_1Btn);    }];
+        make.top.height.width.equalTo(self.ch2_1Btn);
+    }];
+    
+    [self.view addSubview:self.ch4_1Btn];
+    [self.ch4_1Btn makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.ch3_1Btn.right).offset(10);
+        make.top.height.width.equalTo(self.ch2_1Btn);
+    }];
     
     [self.view addSubview:self.demo1Btn];
     [self.demo1Btn makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.ch2_1Btn.bottom).offset(10);
         make.left.height.width.equalTo(self.ch2_1Btn);
+    }];
+    
+    [self.view addSubview:self.demo2Btn];
+    [self.demo2Btn makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.demo1Btn.right).offset(10);
+        make.top.height.width.equalTo(self.demo1Btn);
+    }];
+    
+    [self.view addSubview:self.demoGlsl1Btn];
+    [self.demoGlsl1Btn makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.demo2Btn.right).offset(10);
+        make.top.height.width.equalTo(self.demo1Btn);
     }];
 }
 
@@ -75,6 +100,19 @@
     return _ch3_1Btn;
 }
 
+- (UIButton *)ch4_1Btn{
+    if (!_ch4_1Btn) {
+        UIButton *button = [[UIButton alloc]init];
+        [button setBackgroundImage:nil forState:UIControlStateNormal];
+        [button setTitle:@"Ch4.1" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(ch4_1) forControlEvents:UIControlEventTouchUpInside];
+        [button setImage:[UIImage imageNamed:@"TzClose"] forState:UIControlStateNormal];
+        button.backgroundColor=UIColor.greenColor;
+        _ch4_1Btn = button;
+    }
+    return _ch4_1Btn;
+}
+
 - (UIButton *)demo1Btn{
     if (!_demo1Btn) {
         UIButton *button = [[UIButton alloc]init];
@@ -88,6 +126,32 @@
     return _demo1Btn;
 }
 
+- (UIButton *)demo2Btn{
+    if (!_demo2Btn) {
+        UIButton *button = [[UIButton alloc]init];
+        [button setBackgroundImage:nil forState:UIControlStateNormal];
+        [button setTitle:@"灯光" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(demo2) forControlEvents:UIControlEventTouchUpInside];
+        [button setImage:[UIImage imageNamed:@"TzClose"] forState:UIControlStateNormal];
+        button.backgroundColor=UIColor.brownColor;
+        _demo2Btn = button;
+    }
+    return _demo2Btn;
+}
+
+- (UIButton *)demoGlsl1Btn{
+    if (!_demoGlsl1Btn) {
+        UIButton *button = [[UIButton alloc]init];
+        [button setBackgroundImage:nil forState:UIControlStateNormal];
+        [button setTitle:@"GLSL" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(demoGLSL) forControlEvents:UIControlEventTouchUpInside];
+        [button setImage:[UIImage imageNamed:@"TzClose"] forState:UIControlStateNormal];
+        button.backgroundColor=UIColor.brownColor;
+        _demoGlsl1Btn = button;
+    }
+    return _demoGlsl1Btn;
+}
+
 -(void)ch2_1{
     Ch2_1ViewController *vc=[Ch2_1ViewController new];
     [self.navigationController pushViewController:vc animated:YES];
@@ -98,10 +162,26 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
+-(void)ch4_1{
+    Ch4_1ViewController *vc=[Ch4_1ViewController new];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
 -(void)demo1{
     DrawTriangleViewController *vc=[DrawTriangleViewController new];
     [self.navigationController pushViewController:vc animated:YES];
 }
+
+-(void)demo2{
+    LightGLKVC *vc=[LightGLKVC new];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+-(void)demoGLSL{
+    GLSL1ViewController *vc=[GLSL1ViewController new];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
 
 
 @end
